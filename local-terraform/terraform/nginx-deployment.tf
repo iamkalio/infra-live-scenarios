f3f9acdc648069cf08571f3e9676854f006850e0
kalio007 <kalioprince1@gmail.com>
2025-06-25 23:30:25 +0100

    terraform setup with kind config

---
+++ local-terraform/terraform/nginx-deployment.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
